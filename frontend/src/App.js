282ebe6c8aeee6069c480fec968ff50bffe18d69
juhithaclemens <juhitha_clemens@berkeley.edu>
2025-12-03 17:36:13 -0800

    Quote button added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [quote, setQuote] = useState("Click button for Quote of the Day!");
+
+  const fetchQuote = async () => {
+    const response = await fetch("http://localhost:5000/quote"); // update later for deployment
+    const data = await response.json();
+    setQuote(data.quote);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "40px" }}>
+      <h1>Quote of the Day</h1>
+      <p style={{ fontSize: "20px", fontStyle: "italic" }}>{quote}</p>
+      <button onClick={fetchQuote}>Get Quote</button>
     </div>
   );
 }
